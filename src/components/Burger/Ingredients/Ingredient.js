003566a5d66a82f834731f9646d188b49f8ef9ef
vyeung <vl.yeung@yahoo.com>
2018-05-20 18:25:52 -0400

    added Burger and Ingredient component

---
+++ src/components/Burger/Ingredients/Ingredient.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./Ingredient.css";
+
+const ingredient = (props) => {
+  let ingredient = null;
+
+  if(props.type === "bun-bottom") {
+    ingredient = <div className={styles.BunBottom}></div>;
+  }
+  else if(props.type === "bun-top") {
+    ingredient = (
+      <div className={styles.BunTop}>
+        <div className={styles.Seeds1}></div>
+        <div className={styles.Seeds2}></div>
+      </div>
+    );
+  }
+  else if(props.type === "meat") {
+    ingredient = <div className={styles.Meat}></div>;
+  }
+  else if(props.type === "cheese") {
+    ingredient = <div className={styles.Cheese}></div>;
+  }
+  else if(props.type === "lettuce") {
+    ingredient = <div className={styles.Lettuce}></div>;
+  }
+  else if(props.type === "bacon") {
+    ingredient = <div className={styles.Bacon}></div>;
+  }
+
+  return ingredient;
+}
+
+export default ingredient;
