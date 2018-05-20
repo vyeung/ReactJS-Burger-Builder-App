0ddaae47ad413fe1b3a210114523cc6ac0ae5883
vyeung <vl.yeung@yahoo.com>
2018-05-20 17:38:03 -0400

    added BurgerBuilder class container

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import Aux from "../../hoc/Auxiliary";
+
+class BurgerBuilder extends Component {
+
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>BuildControls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
