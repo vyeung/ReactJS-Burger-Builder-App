48e179c6fd03e03406ace4780e750b5249f9d2ec
vyeung <vl.yeung@yahoo.com>
2018-05-20 23:48:50 -0400

    added ingredients state to BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,19 @@ import Aux from "../../hoc/Auxiliary";
 import Burger from "../../components/Burger/Burger"
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      lettuce: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  }
 
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>BuildControls</div>
       </Aux>
     );
