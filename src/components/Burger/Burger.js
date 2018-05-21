0976d740a1f3064f9757aa00006f3684e2650f17
vyeung <vl.yeung@yahoo.com>
2018-05-20 23:51:48 -0400

    can output burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,24 @@ import styles from "./Burger.css";
 import Ingredient from "./Ingredient/Ingredient";
 
 const burger = (props) => {
+  var ingredientsArray = [];
+
+  for(var key in props.ingredients) {
+    //console.log(key);
+    //console.log(props.ingredients[key]);
+    for(var i=0; i<props.ingredients[key]; i++) {
+      //need to give a key for each element in array. get a warning otherwise
+      //ex: lettuce0, meat0, meat1
+      ingredientsArray.push(<Ingredient key={key+i} type={key} />);
+    }
+  }
+
   return (
-    //burger is a div on the screen
+    //burger is a div on screen. 
+    //will always have a top and bottom bun. put ingredientsArray in between 
     <div className={styles.Burger}>
       <Ingredient type="bun-top" />
-      <Ingredient type="cheese" />
-      <Ingredient type="meat" />
+      {ingredientsArray}
       <Ingredient type="bun-bottom" />
     </div>
   );
