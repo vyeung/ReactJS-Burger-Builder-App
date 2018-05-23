f95423ab84e2a27f016b3f90597740fef07e14ee
vyeung <vl.yeung@yahoo.com>
2018-05-22 23:44:35 -0400

    started NavItems cmpt with css and added to Toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,18 @@
 import React from "react";
 import styles from "./Toolbar.css";
 import Logo from "../../Logo/Logo";
+import NavItems from "../NavItems/NavItems";
 
 const toolbar = (props) => {
   //toolbar is basically a giant header
   return (
     <header className={styles.Toolbar}>
       <div>MENU</div>
+      
       <Logo />
+      
       <nav>
-        ...
+        <NavItems />
       </nav>
     </header>
   );
