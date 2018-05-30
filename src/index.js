ac082aa888790c5112d9a8ad34e0a52e29fe787d
vyeung <vl.yeung@yahoo.com>
2018-05-30 15:38:30 -0400

    added BrowserRouter to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const burgerApp = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+ReactDOM.render(burgerApp, document.getElementById('root'));
 registerServiceWorker();
