88d8396615dc77e6b13fc02425fc869fd44c6bef
vyeung <vl.yeung@yahoo.com>
2018-05-31 16:30:09 -0400

    fetched orders from server and outputted them to MyOrders page

--- src/components/RealOrder/RealOrder.js
+++ src/components/RealOrder/RealOrder.js
@@ -2,10 +2,20 @@ import React from "react";
 import styles from "./RealOrder.css";
 
 const realOrder = (props) => {
+  let realOrderIngreds = [];
+
+  for(var key in props.ingreds) {
+    realOrderIngreds.push(
+      <span key={key} className={styles.Span}>
+        {key} ({props.ingreds[key]})
+      </span>
+    );
+  }
+
   return (
     <div className={styles.RealOrder}>
-      <p>Ingredients</p>
-      <p>Price</p>
+      <p>Ingredients: {realOrderIngreds}</p>
+      <p>Price: <strong>${props.price}</strong></p>
     </div>
   );
 }
--- src/containers/MyOrders/MyOrders.js
+++ src/containers/MyOrders/MyOrders.js
@@ -1,15 +1,43 @@
 import React, {Component} from "react";
 import RealOrder from "../../components/RealOrder/RealOrder";
+import axios from "../../axios-orders";
+import errorHandler from "../../hoc/ErrorHandler/ErrorHandler";
 
 class MyOrders extends Component {
+  state = {
+    orders: [],
+    isLoading: true
+  }
+
+  componentDidMount() {
+    axios.get("./orders.json")
+      .then(response => {
+        const fetchedOrders = [];
+        for(var key in response.data) {
+          //push an object with all the original data and a new id field we made
+          fetchedOrders.push({...response.data[key], id:key});
+        }
+        console.log(fetchedOrders);
+        this.setState({isLoading: false, orders: fetchedOrders});
+      })
+      .catch(error => {
+        this.setState({isLoading: false});
+      });
+  }
+  
   render() {
     return (
       <div>
-        <RealOrder />
-        <RealOrder />
+        {this.state.orders.map(ord => (
+          <RealOrder
+            key={ord.id}
+            ingreds={ord.ingredients}
+            price={ord.price}
+          />
+        ))}
       </div>
     );
   }
 }
 
-export default MyOrders;
+export default errorHandler(MyOrders, axios);
