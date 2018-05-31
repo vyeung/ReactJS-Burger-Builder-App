6cd4dcc838b639d2ad198b499c37523da1553010
vyeung <vl.yeung@yahoo.com>
2018-05-31 00:39:26 -0400

    added Route and NavLink for My Orders page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import MyOrders from "./containers/MyOrders/MyOrders";
 
 class App extends Component {
   render() {
@@ -11,9 +12,7 @@ class App extends Component {
         <Layout>
           <Route path="/" exact component={BurgerBuilder} />
           <Route path="/checkout" component={Checkout} />
-
-          {/* <BurgerBuilder />
-          <Checkout /> */}
+          <Route path="/my-orders" component={MyOrders} />
         </Layout>
       </div>
     );
--- src/components/Navigation/NavItems/NavItems.js
+++ src/components/Navigation/NavItems/NavItems.js
@@ -1,21 +1,22 @@
 import React from "react";
 import styles from "./NavItems.css";
+import {NavLink} from "react-router-dom";
 
 const navItems = () => {
   return (
     <ul className={styles.NavItems}>
       <li className={styles.Item}>
-        <a 
-          href="/"
-          //className={props.active ? styles.active : null}
-          className={styles.active}
-          >Burger Builder</a>
+        <NavLink
+          to="/"
+          exact
+          activeClassName={styles.active}  //Wouldn't need to do this if not using CSS Modules
+          >Burger Builder</NavLink>
       </li>
       <li className={styles.Item}>
-        <a 
-          href="/"
-          //className={props.active ? styles.active : null}
-          >Checkout</a>
+        <NavLink 
+          to="/my-orders"
+          activeClassName={styles.active}
+          >My Orders</NavLink>
       </li>
     </ul>
   );
