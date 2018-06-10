11c594394b3c251a5530671171fcade052015c05
vyeung <vl.yeung@yahoo.com>
2018-06-10 16:37:47 -0400

    created redux store in index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import reducer from "./store/reducers/reducer";
+
+const myStore = createStore(reducer);
 
 const burgerApp = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={myStore}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(burgerApp, document.getElementById('root'));
