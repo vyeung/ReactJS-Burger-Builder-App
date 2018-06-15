15e5aab401b0977043b020933b37a3ed4f987e5b
vyeung <vl.yeung@yahoo.com>
2018-06-15 17:28:39 -0400

    added Logout route and redirect

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import MyOrders from "./containers/MyOrders/MyOrders";
 import Auth from "./containers/Auth/Auth";
+import Logout from "./containers/Auth/Logout/Logout";
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
           <Route path="/checkout" component={Checkout} />
           <Route path="/my-orders" component={MyOrders} />
           <Route path="/auth" component={Auth} />
+          <Route path="/logout" component={Logout} />
         </Layout>
       </div>
     );
