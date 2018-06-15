cd4be6e45cfbfc27027de72a9bc1045fa00ba6ae
vyeung <vl.yeung@yahoo.com>
2018-06-15 19:08:21 -0400

    only logged in users can see orders page

--- src/components/Navigation/NavItems/NavItems.js
+++ src/components/Navigation/NavItems/NavItems.js
@@ -12,11 +12,16 @@ const navItems = (props) => {
           activeClassName={styles.active}  //Wouldn't need to do this if not using CSS Modules
           >Burger Builder</NavLink>
       </li>
+
+      {/*only logged in users can see orders page*/}
       <li className={styles.Item}>
-        <NavLink 
-          to="/my-orders"
-          activeClassName={styles.active}
-          >My Orders</NavLink>
+        {props.isAuthenticated===true ? 
+          <NavLink 
+            to="/my-orders"
+            activeClassName={styles.active}
+            >My Orders</NavLink>
+          : null
+        }
       </li>
 
       {/* inline using ternary operator */}
